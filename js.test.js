71f3b514d913f5509c104ed69163fda2ad89c62e
Your Name <Hubrisy>
2022-02-01 17:22:55 +0200

    js

---
+++ js.test.js
@@ -0,0 +1,26 @@
+const inputOne = document.getElementById('inputOne');
+const inputTwo = document.getElementById('inputTwo');
+const inputThree = document.getElementById('inputThree');
+const inputFour = document.getElementById('inputFour');
+const inputFive = document.getElementById('inputFive');
+
+const button = document.getElementById('register');
+
+// let newArr = [];
+
+// function getInfo(){
+//     newArr.push(inputOne.value,inputTwo.value,inputThree.value,inputFour.value,inputFive.value);
+//     console.log(newArr);
+// }
+
+// button.addEventListener('click',getInfo);
+
+function getInfo(){
+    console.log("Name:",inputOne.value)
+    console.log("Last Name:",inputTwo.value)
+    console.log("Email:",inputThree.value)
+    console.log("Artist:",inputFour.value)
+    console.log("Country:",inputFive.value)
+}
+
+button.addEventListener('click',getInfo);
